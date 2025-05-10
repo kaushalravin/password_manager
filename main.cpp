7c507a7307fd0fbf716990d74b130c39e29857e4
Kaushal N <kaushalravin007@gmail.com>
2025-05-10 09:22:09 +0530

    for demo of working

--- main.cpp
+++ main.cpp
@@ -5,6 +5,18 @@ using namespace std;
 
 int main(){
     Password a;
+    string mas;
+    cout<<"enter master password:";
+    getline(cin,mas);
+    a.getmaster();
+    string master=a.return_master();
+    if(master==mas){
     a.getdata();
+    a.writedata();
+    a.readdata();
+    a.putdata();
+    a.delete_line();
+    a.modify();}
+ 
     return 0;
 }
